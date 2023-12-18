1776b6e211ed2e8ff7af56156993c06e44fb8768
shree <shreesbadiger@gmail.com>
2023-12-18 14:15:11 +0530

    updated

---
+++ vars.tf
@@ -0,0 +1,6 @@
+variable "parameters" {
+  default = [
+    { name = "dev.rds.username", type = "String", value = "admin1"},
+    { name = "dev.rds.password", type = "SecureString", value = "ExpenseApp1"},
+            ]
+  }
