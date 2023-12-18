6dfd85905450d80a102eea433dba397b789386d3
shree <shreesbadiger@gmail.com>
2023-12-18 13:51:45 +0530

    hkh

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-remote-d76"
+    key    = "parameter-store/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+variable "parameters" {
+  default = [
+    { name = "dev.rds.username", type = "string", value = "admin1"},
+    { name = "dev.rds.password", type = "securestring", value = "ExpenseApp1"},
+  ]
+  }
+
+  resource "aws_ssm_parameter" "params" {
+    count = length(var.parameters)
+    name = var.parameters[count.index].name
+    type = var.parameters[count.index].type
+    value = var.parameters[count.index].value
+    
+  }
