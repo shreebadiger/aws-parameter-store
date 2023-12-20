4afff9219a4c7fdac56b4487194a9722702a0a80
shree <shreesbadiger@gmail.com>
2023-12-21 00:52:37 +0530

    updated

--- vars.tf
+++ vars.tf
@@ -1,8 +1,8 @@
 variable "parameters" {
     default = [
         
-        { name = "dev.backend.DB_HOST", type = "SecureString", value = "terraform-20231220162137043200000002.cza2eiiicaa6.us-east-1.rds.amazonaws.com"},
-        { name = "dev.rds.endpoint", type = "SecureString", value = "terraform-20231220162137043200000002.cza2eiiicaa6.us-east-1.rds.amazonaws.com"},
+        { name = "dev.backend.DB_HOST", type = "SecureString", value = "terraform-20231220190502900700000002.cza2eiiicaa6.us-east-1.rds.amazonaws.com"},
+        { name = "dev.rds.endpoint", type = "SecureString", value = "terraform-20231220190502900700000002.cza2eiiicaa6.us-east-1.rds.amazonaws.com"},
         { name = "dev.rds.username", type = "String", value = "admin1"},
         { name = "dev.rds.password", type = "SecureString", value = "ExpenseApp1"}
 
