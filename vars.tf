ccad6db20bd5205b18991635b91d9191415e2c58
shree <shreesbadiger@gmail.com>
2023-12-24 01:38:59 +0530

    updated

--- vars.tf
+++ vars.tf
@@ -1,11 +1,11 @@
 variable "parameters" {
     default = [
         
-        { name = "dev.backend.DB_HOST", type = "String", value = "terraform-20231223100145487000000002.cza2eiiicaa6.us-east-1.rds.amazonaws.com"},
-        { name = "dev.rds.endpoint", type = "SecureString", value = "terraform-20231223100145487000000002.cza2eiiicaa6.us-east-1.rds.amazonaws.com"},
-         { name = "dev.frontend.BACKEND_ENDPOINT", type = "SecureString", value = "http://backend-dev.sbadiger93.online/"},
-        { name = "dev.rds.username", type = "String", value = "admin1"},
-        { name = "dev.rds.password", type = "SecureString", value = "ExpenseApp1"}
+    { name = "dev.backend.DB_HOST", type = "String", value = "terraform-20231223195314176000000002.cza2eiiicaa6.us-east-1.rds.amazonaws.com"},
+    { name = "dev.rds.endpoint", type = "SecureString", value = "terraform-20231223195314176000000002.cza2eiiicaa6.us-east-1.rds.amazonaws.com"},
+    { name = "dev.frontend.BACKEND_ENDPOINT", type = "SecureString", value = "http://backend-dev.sbadiger93.online/"},
+    { name = "dev.rds.username", type = "String", value = "admin1"},
+    { name = "dev.rds.password", type = "SecureString", value = "ExpenseApp1"}
 
     ]   
 }
